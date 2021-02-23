40a04b680be194197a74433108c4b28516f72d2f
Dmitry Khaleev <dkhaleev@gmail.com>
2021-02-23 23:43:16 +0200

    Display center frequency via shift register by timer

--- Arduino/remote/remote.ino
+++ Arduino/remote/remote.ino
@@ -1,6 +1,7 @@
 /* SDRuno Bridge plugin mocking firmware v0.1
  *
  */
+#include <TimerOne.h>
 
 struct __attribute__((__packed__)) State {
   int8_t      Demodulator             = 0; //Demodulator type,        Enum:
@@ -37,12 +38,12 @@ struct __attribute__((__packed__)) State {
   int8_t      CwPeakFilterThreshold   = 0;
   int8_t      AudioVolume             = 0;
   int8_t      SP1MinPower             = 0;
-  long int    VfoFrequency            = 0; //VFO Freq,                Hz
-  long int    CenterFrequency         = 443300000; //Center Frequency,        Hz
+  long int    VfoFrequency            = 0;          //VFO Freq,                Hz
+  long int    CenterFrequency         = 443300000;  //Center Frequency,        Hz
   long int    SP1MinFrequency         = 0;
   long int    SP1MaxFrequency         = 0;
   long int    MPXLevel                = 0;
-  int         FilterBandwidth         = 4300; //Filter bandwith,         Hz
+  int         FilterBandwidth         = 4300;       //Filter bandwith,         Hz
   int         SquelchLevel            = 0;
   bool        SquelchEnable           = false;
   bool        FmNoiseReductionEnable  = false;
@@ -51,11 +52,49 @@ struct __attribute__((__packed__)) State {
   long int    fingerprint;
 } state;
 
+//LED params
+// pin 11 of 74HC595 (SHCP)
+const int bit_clock_pin = 3;
+// pin 12 of 74HC595 (STCP)
+const int digit_clock_pin = 4;
+// pin 14 of 74HC595 (DS)
+const int data_pin = 2;
+
+// digit pattern for a 7-segment display
+const byte digit_pattern[16] =
+{
+  B00111111,  // 0
+  B00000110,  // 1
+  B01011011,  // 2
+  B01001111,  // 3
+  B01100110,  // 4
+  B01101101,  // 5
+  B01111101,  // 6
+  B00000111,  // 7
+  B01111111,  // 8
+  B01101111,  // 9
+  B01110111,  // A
+  B01111100,  // b
+  B00111001,  // C
+  B01011110,  // d
+  B01111001,  // E
+  B01110001   // F
+};
+
 long lastRandomSent;
 
 void setup() {
   Serial.begin(9600);
   State state;
+
+//LED pins setup
+  pinMode(data_pin,         OUTPUT);
+  pinMode(bit_clock_pin,    OUTPUT);
+  pinMode(digit_clock_pin,  OUTPUT);
+
+//Timer interrupt for display freq;
+  Timer1.initialize(1000000); //timing for 1s
+  Timer1.attachInterrupt(showCenterFrequencyLED);
 }
 
 void loop() {
@@ -63,13 +102,13 @@ void loop() {
 
   lastRandomSent = state.fingerprint;
   updateDemodulator();
-//  updateVfoFrequency();
+  updateVfoFrequency();
   updateCenterFrequency();
   updateFilterBandwidth();
+  
+  echoStruct();  
 
-  echoStruct();
-
-  delay(1000); //wait 1000 mS
+//  delay(1000); //wait 1000 mS
   String str = Serial.readStringUntil('\n');
 
 //  if (str.length() > 0) {
@@ -143,6 +182,20 @@ void parseStruct(String string) {
          &state.CenterFrequency,
          &state.FilterBandwidth,
          &state.fingerprint);
+}
+
+void showCenterFrequencyLED(){
+  int array[12];
+  long int number = state.CenterFrequency;
+
+ digitalWrite(digit_clock_pin, LOW);
+  for (int i = 11; i >= 0; i--) {
+    byte pattern = digit_pattern[number % 10];
+    shiftOut(data_pin, bit_clock_pin, MSBFIRST, ~pattern);
+      array[i] = number % 10;
+      number /= 10;
+  }
+  digitalWrite(digit_clock_pin, HIGH);
 }
 
 void updateDemodulator() {
